7bc92a40159157978f3b131e6ce02901b22c6fd2
bitty shatto <bittyshatto@gmail.com>
2024-03-06 11:33:45 +0530

    first commit

---
+++ lib/views/home.dart
@@ -0,0 +1,56 @@
+import 'package:final_project/controllers/auth_services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Home Page")),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  maxRadius: 32,
+                  child: Text(FirebaseAuth.instance.currentUser!.email
+                      .toString()[0]
+                      .toUpperCase()),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(FirebaseAuth.instance.currentUser!.email.toString())
+              ],
+            )),
+            ListTile(
+              onTap: () {
+                AuthService().logout();
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text("Logged Out")));
+                Navigator.pushReplacementNamed(context, "/login");
+              },
+              leading: Icon(Icons.logout_outlined),
+              title: Text("Logout"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
